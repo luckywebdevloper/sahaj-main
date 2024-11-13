7956ae57358329c84f8a75b1f5f378bec350c166
ROHAN SINGH <digitalsolutionindore@gmail.com>
2024-11-13 12:32:48 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    amaan
+      amaan
+      <Button>click</Button>
     </>
   );
 }
